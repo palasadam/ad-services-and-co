1c086d2fe75a9eaeab689ce1564c2ac30f692eb2
palasadam <omer.cod@gmail.com>
2024-05-29 22:25:34 +0200

    last

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,43 +1,31 @@
 import React from "react";
 import Section from "./Section";
-import imageWhiteboard from "@/images/whiteboard.jpg";
+import imageWhiteboard from "@/images/foto1.jpeg";
 import { TagList, TagListItem } from "./TagList";
 
 const Discover = () => {
   return (
-    <Section title="Discover" image={{ src: imageWhiteboard, shape: 1 }}>
+    <Section title="SAY CHEEESE!" image={{ src: imageWhiteboard, shape: 1 }}>
       <div className="space-y-6 text-base text-neutral-600">
         <p>
-          We work closely with our clients to understand their{" "}
-          <strong className="font-semibold text-neutral-950">needs</strong> and
-          goals, embedding ourselves in their every day operations to understand
-          what makes their business tick.
-        </p>
-        <p>
-          Our team of private investigators shadow the company director’s for
-          several weeks while our account managers focus on going through their
-          trash. Our senior security experts then perform social engineering
-          hacks to gain access to their{" "}
-          <strong className="font-semibold text-neutral-950">business</strong>
-          accounts — handing that information over to our forensic accounting
-          team.
-        </p>
-        <p>
-          Once the full audit is complete, we report back with a comprehensive
-          <strong className="font-semibold text-neutral-950">plan</strong> and,
-          more importantly, a budget.
+          Avec un photographe professionnel, on organise une séance photo de
+          votre appartement afin de rédiger une annonce attractive sur les
+          différentes plateformes.{" "}
+          <strong className="font-semibold text-neutral-950">
+            Notre équipe communication{" "}
+          </strong>
+          s&apos;occupe de la rédaction de l&apos;annonce à la gestion des
+          réservations et demandes d&apos;informations quotidiennes.
         </p>
       </div>
       <h3 className="mt-12 font-display text-base font-semibold text-neutral-950">
-        Included in this phase
+        Les voyageurs ont à disposition une personne totalement à l&apos;écoute
+        avant, pendant et lors de la fin de leur séjour.{" "}
       </h3>
       <TagList className="mt-4">
-        <TagListItem>In-depth questionnaires</TagListItem>
-        <TagListItem>Feasibility studies</TagListItem>
-        <TagListItem>Blood samples</TagListItem>
-        <TagListItem>Employee surveys</TagListItem>
-        <TagListItem>Proofs-of-concept</TagListItem>
-        <TagListItem>Forensic audit</TagListItem>
+        <TagListItem>test 1</TagListItem>
+        <TagListItem>test 1</TagListItem>
+        <TagListItem>test 1</TagListItem>
       </TagList>
     </Section>
   );
